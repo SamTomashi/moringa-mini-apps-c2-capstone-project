9ffedffd28d1cfa4732f804cb0f211e7cc57f309
Sam Tomashi <samtomashikayembi@yahoo.fr>
2024-05-16 12:19:08 +0300

    Create vehicle constructors

---
+++ src/Vehicle.js
@@ -0,0 +1,14 @@
+export function Car(model, make, year){
+    this.model = model;
+    this.make = make;
+    this.year = year;
+    
+}
+
+export function MotorBike(){
+
+}
+
+export function Train(){
+
+}
